4f297c543aa2738e5ddeab2e42c0ae32e8f4f120
Nikolay Dinkov <n.z.dinkov@gmail.com>
2022-02-14 07:28:46 +0200

    Added length calculation maybe we will need threads so it can be faster

--- BikeNav/FileWork/WayNew.swift
+++ BikeNav/FileWork/WayNew.swift
@@ -6,25 +6,40 @@
 //
 
 import Foundation
+import CoreLocation
 
 
 struct WayNew {
     let id: Int
     let keyVal: [String: String]
     let nodeRefs: [Int] // MARK: Maybe I can use here the whole struct DenseNodeNew so when calculating the lenght it could be faster and also this may help removing the whole array in the RawFile file
-    let lenght: Double
+    var length: Double
     
     init(id: Int, keyVal: [String: String], nodeRefs: [Int]) {
         self.id = id
         self.keyVal = keyVal
         self.nodeRefs = nodeRefs
-        self.lenght = 0
+        self.length = 0.0
     }
     
-    func calculateLenght() {
-        // MARK: Calc the lenght
+    mutating func calculateLength(nodes: [DenseNodeNew]) {
+        // MARK: Calc the lenght | Maybe we can use threads and maybe use only one variable when getting from nodes by id
+        var previousLat: Double = 0.0
+        var previousLon: Double = 0.0
         for nodeId in nodeRefs {
+            guard let idx = nodes.firstIndex(where: { nodeId == $0.id }) else {
+                print("No id of node which is bad")
+                assert(false)
+            }
             
+            if previousLat == 0.0 && previousLon == 0.0 {
+                previousLat = nodes[idx].latitude
+                previousLon = nodes[idx].longitude
+            } else {
+                let location = CLLocation(latitude: nodes[idx].latitude, longitude: nodes[idx].longitude)
+                let distance = location.distance(from: CLLocation(latitude: previousLat, longitude: previousLon))
+                self.length = self.length + distance
+            }
         }
     }
 }
